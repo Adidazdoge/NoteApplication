2b2d19c8373e5475869ec49d947ea29c1e597389
Michael Lin <lin@MichaeldeMacBook-Pro-2.local>
2024-11-25 19:56:28 -0500

    Implement Broadcast use case for player actions

---
+++ src/main/java/usecases/broadcast/BroadcastOutputData.java
@@ -0,0 +1,34 @@
+package usecases.broadcast;
+
+/**
+ * Output data for the broadcast use case.
+ */
+public class BroadcastOutputData {
+    private final String resultMessage;
+    private final int survivorsFound;
+    private final int resourcesFound;
+    private final boolean attractedZombies;
+
+    public BroadcastOutputData(String resultMessage, int survivorsFound, int resourcesFound, boolean attractedZombies) {
+        this.resultMessage = resultMessage;
+        this.survivorsFound = survivorsFound;
+        this.resourcesFound = resourcesFound;
+        this.attractedZombies = attractedZombies;
+    }
+
+    public String getResultMessage() {
+        return resultMessage;
+    }
+
+    public int getSurvivorsFound() {
+        return survivorsFound;
+    }
+
+    public int getResourcesFound() {
+        return resourcesFound;
+    }
+
+    public boolean isAttractedZombies() {
+        return attractedZombies;
+    }
+}
