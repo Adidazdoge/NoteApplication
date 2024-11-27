7b84548a697cea0237a357f910d7d73948578dc8
Michael Lin <lin@MichaeldeMacBook-Pro-2.local>
2024-11-27 02:21:59 -0500

    Implement JsonPlayerRepository for managing player data using JSON files

--- src/main/java/frameworks/database/JsonPlayerRepository.java
+++ src/main/java/frameworks/database/JsonPlayerRepository.java
@@ -1,4 +1,45 @@
 package frameworks.database;
 
-public class JsonPlayerRepository {
+import java.io.IOException;
+import java.util.List;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import entities.Player;
+import interface_adapters.gateways.PlayerRepository;
+
+/**
+ * A JSON-based implementation of the PlayerRepository interface.
+ */
+public class JsonPlayerRepository implements PlayerRepository {
+    private final FileDatabase<Player> database;
+    private List<Player> players;
+
+    public JsonPlayerRepository(String filePath) throws IOException {
+        this.database = new FileDatabase<>(filePath, new TypeReference<List<Player>>() {});
+        this.players = database.load();
+    }
+
+    @Override
+    public Player findByUsername(String username) {
+        return players.stream()
+                .filter(player -> player.getUsername().equals(username))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public boolean isUsernameDuplicate(String username) {
+        return findByUsername(username) != null;
+    }
+
+    @Override
+    public void addPlayer(Player player) {
+        players.add(player);
+        try {
+            database.save(players);
+        }
+        catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
 }
