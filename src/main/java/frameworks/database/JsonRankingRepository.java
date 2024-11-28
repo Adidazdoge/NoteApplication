3e1cfa47bb89f9cb4b0e195b0ace9a1a51363e75
Michael Lin <lin@MichaeldeMacBook-Pro-2.local>
2024-11-28 18:07:29 -0500

    Resolving formatting error

--- src/main/java/frameworks/database/JsonRankingRepository.java
+++ src/main/java/frameworks/database/JsonRankingRepository.java
@@ -5,32 +5,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import entities.RankingEntry;
+import entities.PlayerRankingEntry;
 import interface_adapters.gateways.RankingRepository;
 
 /**
  * A JSON-based implementation of the RankingRepository interface.
  */
+
 public class JsonRankingRepository implements RankingRepository {
-    private final FileDatabase<RankingEntry> database;
-    private List<RankingEntry> rankings;
+    private final FileDatabase<PlayerRankingEntry> database;
+    private List<PlayerRankingEntry> rankings;
 
     public JsonRankingRepository(String filePath) throws IOException {
-        this.database = new FileDatabase<>(filePath, new TypeReference<List<RankingEntry>>() {});
+        this.database = new FileDatabase<>(filePath, new TypeReference<List<PlayerRankingEntry>>() { });
         this.rankings = database.load();
     }
 
     @Override
-    public List<RankingEntry> getAllRankings() {
+    public List<PlayerRankingEntry> getAllRankings() {
         return new ArrayList<>(rankings);
     }
     /**
      * Adds a new ranking entry to the repository and persists it to the database.
      *
-     * @param rankingEntry The {@link RankingEntry} object to be added to the rankings list.
+     * @param rankingEntry The {@link PlayerRankingEntry} object to be added to the rankings list.
      */
 
-    public void addRanking(RankingEntry rankingEntry) {
+    public void addRanking(PlayerRankingEntry rankingEntry) {
         rankings.add(rankingEntry);
         try {
             database.save(rankings);
