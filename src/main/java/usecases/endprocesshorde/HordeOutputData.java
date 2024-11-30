e91db88cbf86f203a19c09d19853dca7a0502acf
Adidazdoge <owenn.wang@mail.utoronto.ca>
2024-11-29 17:12:54 -0700

    Horde usecase implemented

--- src/main/java/usecases/endprocesshorde/HordeOutputData.java
+++ src/main/java/usecases/endprocesshorde/HordeOutputData.java
@@ -1,4 +1,22 @@
 package usecases.endprocesshorde;
 
+/**
+ * Output data, consist of final outcome description, player's score.
+ */
 public class HordeOutputData {
+    private int score;
+    private String description;
+
+    public HordeOutputData(int score, String description) {
+        this.score = score;
+        this.description = description;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
