b2ff13cfae67ea97b3ab9c76d5bced3505c2a791
FlashYDirOX <flashydirox@gmail.com>
2024-11-30 02:31:10 -0500

    Blizzard Event Rewritten

--- src/main/java/usecases/eventrespond/blizzard/BlizzardInteractor.java
+++ src/main/java/usecases/eventrespond/blizzard/BlizzardInteractor.java
@@ -11,14 +11,31 @@ import usecases.eventrespond.RespondOutputData;
  */
 public class BlizzardInteractor {
 
-    public Pair<RespondOutputData, Boolean> evaluate(Inventory inventory, EventBlizzard event) {
-        String message = event.getOutcome();
-        int foodChange = EntityConstants.BLIZZARDRESOURCELOSSFOOD;
-        int waterChange = EntityConstants.BLIZZARDRESOURCELOSSWATER;
+    public Pair<RespondOutputData, Boolean> evaluate(int choice, Inventory inventory, EventBlizzard event) {
+        String message = "";
+        int foodChange = 0, waterChange = 0;
+        boolean success = true;
+
+        if (choice == EntityConstants.FIRSTCHOICE) { // Fight (Secure shelter)
+            foodChange = EntityConstants.BLIZZARDRESOURCELOSSFOOD / 2; // Halve the resource loss
+            waterChange = EntityConstants.BLIZZARDRESOURCELOSSWATER / 2;
+            message = event.getFightOutcome();
+        } else if (choice == EntityConstants.SECONDCHOICE) { // Negotiate (Prepare supplies)
+            foodChange = EntityConstants.BLIZZARDRESOURCELOSSFOOD; // Normal resource loss
+            waterChange = EntityConstants.BLIZZARDRESOURCELOSSWATER;
+            message = event.getNegotiateOutcome();
+        } else if (choice == EntityConstants.THIRDCHOICE) { // Flee (Do nothing)
+            foodChange = EntityConstants.BLIZZARDRESOURCELOSSFOOD * 2; // Double the resource loss
+            waterChange = EntityConstants.BLIZZARDRESOURCELOSSWATER * 2;
+            message = event.getFleeOutcome();
+        } else {
+            success = false;
+            message = "Invalid choice.";
+        }
 
         inventory.changeFood(foodChange);
         inventory.changeWater(waterChange);
 
-        return new Pair<>(new RespondOutputData(message, foodChange, waterChange, 0, 0, ""), true);
+        return new Pair<>(new RespondOutputData(message, foodChange, waterChange, 0, 0, ""), success);
     }
 }
